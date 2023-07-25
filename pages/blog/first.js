106d483633a25f082567044c6d29678a5c290d44
Bimon <130655556+bimon08@users.noreply.github.com>
2023-07-25 21:41:33 +0530

    next

---
+++ pages/blog/first.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function FirstBlog() {
+  return <h1>FirstBlog Page</h1>;
+}
+
+export default FirstBlog;
---
+++ pages/blog/second.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SecondBlog() {
+  return <h1>SecondBlog Page</h1>;
+}
+
+export default SecondBlog;
